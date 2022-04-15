a6aa1be85d34291cc9342a62877f6b4530bee8a5
Rai <raibr16@gmail.com>
2022-04-15 15:15:10 +0200

    Updates

---
+++ precache-manifest.45be001d72f92b5544a3e8ec5926fbbf.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "fc3f228e4bfd2fbe2376",
+    "url": "/Dog-Day-Care-V2/static/css/main.77d7049a.chunk.css"
+  },
+  {
+    "revision": "fc3f228e4bfd2fbe2376",
+    "url": "/Dog-Day-Care-V2/static/js/main.fc3f228e.chunk.js"
+  },
+  {
+    "revision": "3e0bcebd8658b94a8bd9",
+    "url": "/Dog-Day-Care-V2/static/js/1.3e0bcebd.chunk.js"
+  },
+  {
+    "revision": "e382fedbdff9ce8ac9bd",
+    "url": "/Dog-Day-Care-V2/static/js/runtime~main.e382fedb.js"
+  },
+  {
+    "revision": "d3e4a4b8868f8b059443bdfda880cb09",
+    "url": "/Dog-Day-Care-V2/static/media/backgrund.d3e4a4b8.mp4"
+  },
+  {
+    "revision": "862e69ecc9a7deeba5076bad0456308a",
+    "url": "/Dog-Day-Care-V2/static/media/dog.862e69ec.jpg"
+  },
+  {
+    "revision": "3d6e40e234072d7ac69726a7cb7f5fe2",
+    "url": "/Dog-Day-Care-V2/static/media/care.3d6e40e2.jpg"
+  },
+  {
+    "revision": "dc02ccd6aacfe8db5ed55dfd8dd48c19",
+    "url": "/Dog-Day-Care-V2/static/media/dog2.dc02ccd6.jpg"
+  },
+  {
+    "revision": "9f105d1e543e0114efcec8d09cbb162a",
+    "url": "/Dog-Day-Care-V2/static/media/dog3.9f105d1e.jpg"
+  },
+  {
+    "revision": "af8bb4db27430503ea0c97e480e37183",
+    "url": "/Dog-Day-Care-V2/static/media/dog4.af8bb4db.jpg"
+  },
+  {
+    "revision": "a4daef0b9901009a74b44155e1ca12f3",
+    "url": "/Dog-Day-Care-V2/static/media/back3.a4daef0b.mp4"
+  },
+  {
+    "revision": "98546e8bd6650e79dcfda4bd617fe470",
+    "url": "/Dog-Day-Care-V2/static/media/back2.98546e8b.mp4"
+  },
+  {
+    "revision": "8f538dfbf9d46efb14ebf025a56bb05c",
+    "url": "/Dog-Day-Care-V2/index.html"
+  }
+];
