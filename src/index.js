c33dc624f840f106fdf597685016c250298138fe
Rai <raibr16@gmail.com>
2022-04-15 16:43:32 +0200

    Icon test

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ import { BrowserRouter } from 'react-router-dom';
 
 
 ReactDOM.render( <
-    BrowserRouter >
-    <
-    App / >
-    <
-    /BrowserRouter >, document.getElementById('root'));
+        BrowserRouter >
+        <
+        App / >
+        <
+        /BrowserRouter >, document.getElementById('root'));
